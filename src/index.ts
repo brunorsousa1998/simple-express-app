e2bc3bd6ac0233f1ee6e2e77d8a9915f3272fc52
Bruno <bravo@Echo.localdomain>
2022-08-23 09:21:54 -0300

    Create default route

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import * as express from 'express';
 const app = express();
 const port = process.env.port || '5000';
 
+app.use(express.json());
+
+app.get('/', (req, res) => res.send({message: 'ok'}));
+
 app.listen(port, () => console.log(`App is listening on port ${port}`));
